319de026f1076cf7f5bb11a928c9ff28acd7a3f5
zhaoyc <zhaoyc@ebikj.com>
2019-04-25 14:43:33 +0800

    init

---
+++ suxia-toshow-webapp/src/main/java/com/suxia/toshow/WebApplication.java
@@ -0,0 +1,19 @@
+package com.suxia.toshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoyc@ebikj.com
+ * @version v_1.0.0
+ * @date Created in 2019年04月23 13:38 星期二
+ * @description TODO
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
